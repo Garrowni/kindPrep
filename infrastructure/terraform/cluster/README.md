0bc62da09cdb5d9d98aadbad289894dec34573f1
garrowni <nicole.garrow64@gmail.com>
2023-10-29 18:01:06 -0400

    added in a read me with links and the providers

---
+++ infrastructure/terraform/cluster/README.md
@@ -0,0 +1,7 @@
+https://registry.terraform.io/providers/tehcyx/kind/latest/docs/resources/cluster
+
+https://registry.terraform.io/providers/hashicorp/kubernetes/latest
+
+https://registry.terraform.io/providers/hashicorp/helm/latest
+
+https://registry.terraform.io/providers/hashicorp/null/latest
---
+++ infrastructure/terraform/cluster/providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    kind = {
+      source = "tehcyx/kind"
+      version = "0.2.1"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.23.0"
+    }
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.11.0"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.1"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
+
+provider "kind" {}
